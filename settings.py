8a1c7ce2da857671d78bfa143e18acc054bd029d
Kerem Berk Güçlü <32224387+keremperq@users.noreply.github.com>
2021-01-20 02:27:48 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,11 +1,4 @@
 DEBUG = True
 PORT = 8080
-SECRET_KEY = "secret"
+SECRET_KEY = "8b7JpWVgQ6stWsC5"
 WTF_CSRF_ENABLED = False
-
-PASSWORDS = {
-    "admin": "$pbkdf2-sha256$29000$PIdwDqH03hvjXAuhlLL2Pg$B1K8TX6Efq3GzvKlxDKIk4T7yJzIIzsuSegjZ6hAKLk",
-    "normaluser": "$pbkdf2-sha256$29000$Umotxdhbq9UaI2TsnTMmZA$uVtN2jo0I/de/Kz9/seebkM0n0MG./KGBc1EPw5X.f0"
-}
-
-ADMIN_USERS = ["admin"]
