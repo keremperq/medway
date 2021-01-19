fe65ac507f7e34758065b5c6071a5a9f9b1bf38a
Kerem Berk Güçlü <32224387+keremperq@users.noreply.github.com>
2021-01-19 23:37:59 +0300

    Add files via upload

--- views/category_view.py
+++ views/category_view.py
@@ -2,15 +2,15 @@ from flask import current_app, render_template, request, redirect, url_for
 from views.equipment_view import take_equipment_ids_and_names_by_equipment, take_categories_by_equipment
 
 
-def equipments_by_category_page(category_id):
+def equipments_by_category_page(cat_id):
     db = current_app.config["db"]
-    category = db.category.get_row(where_columns="CATEGORY_ID", where_values=category_id)
+    category = db.category.get_row(where_columns="CAT_ID", where_values=cat_id)
     if request.method == "GET":
-        equipment_category_list = db.category.get_table(where_columns="CATEGORY_ID", where_values=category_id)
+        equipment_category_list = db.category.get_table(where_columns="CAT_ID", where_values=cat_id)
         equipments = []
         for equipment_category in equipment_category_list:
             equipments.append((db.equipment.get_row(equipment_category.eq_id), take_equipment_ids_and_names_by_equipment(equipment_category.eq_id), take_categories_by_equipment(equipment_category.eq_id)))
-        return render_template("equipment/equipments.html", equipments=equipments, title="Equipments in category of " + category.category_name)
+        return render_template("equipment/equipments.html", equipments=equipments, title="Equipments in category of " + category.cat_name)
     else:
         form_equipment_keys = request.form.getlist("equipment_keys")
         for form_equipment_key in form_equipment_keys:
